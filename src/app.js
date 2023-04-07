e7a0c7f00b3f033687418b8289e7857f895e302b
teapotpiggy <129310192+teapotpiggy@users.noreply.github.com>
2023-04-06 23:44:17 -0500

    JS dates and hours

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
-function displayTemperature(response) {
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        `0${hours}`;
+    }
 
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let day = days[date.getDay()];
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -14,10 +31,11 @@ function displayTemperature(response) {
 
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = response.data.main.wind.speed;
-}
 
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
 let apiKey = "517be2430t72605eaa2f77c918a3o96d";
 let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Lagos&key=517be2430t72605eaa2f77c918a3o96d&units=metric";
-
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
